48c21e08bdd9aa4aefcf637d953458671ab33f8a
李少锋 <wb59@yingu.com>
2019-06-04 15:23:34 +0800

    Initial Commit

---
+++ DragView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DragView
+//
+//  Created by 李少锋 on 2019/6/4.
+//  Copyright © 2019年 李少锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DragView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DragView
+//
+//  Created by 李少锋 on 2019/6/4.
+//  Copyright © 2019年 李少锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
