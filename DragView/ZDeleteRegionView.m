bb733cf5a947110ebead9610b413bdc8a6b0d887
李少锋 <wb59@yingu.com>
2019-06-05 17:14:58 +0800

    选取照片，对照片实现拖动排序，非collectionView实现

---
+++ DragView/ZDeleteRegionView.m
@@ -0,0 +1,21 @@
+//
+//  ZDeleteRegionView.m
+//  DragView
+//
+//  Created by 李少锋 on 2019/6/5.
+//  Copyright © 2019年 李少锋. All rights reserved.
+//
+
+#import "ZDeleteRegionView.h"
+
+@implementation ZDeleteRegionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DragView
+# DragView
